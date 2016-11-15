d6dbafe598e2399c2a11eaa04998c6d22aa5d8be
WolfGrin <sblazze@gmail.com>
2016-11-15 17:26:17 +0200

    My new Java project (v1 release)

--- src/pkg1_application/Main.java
+++ src/pkg1_application/Main.java
@@ -16,6 +16,22 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String firstName = "Дмитрий";
+        String middleName = "Сергеевич";
+        String lastName = "Михайлик";
+        String FIO = firstName + " " + middleName + " " + firstName;
+        String dr = "30.05.1985";
+        
+        System.out.println("Михайлик Дмитрий Сргеевич");
+        System.out.println(FIO);
+        System.out.println(dr);
+        System.out.println(FIO + " " + dr);
+                
+        //однострочный комментарий
+        /* многострочный
+        комментарий
+        */
+        
     }
     
 }
